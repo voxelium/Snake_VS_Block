869dd226e55a32e31e0f5d5b4a67a4f85a640b25
VOXELIUM <52653488+voxelium@users.noreply.github.com>
2023-04-04 09:51:23 +0400

    Set random colors block

--- Assets/Scripts/Block/Block.cs
+++ Assets/Scripts/Block/Block.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof (SpriteRenderer))]
+
 public class Block : MonoBehaviour
 {
     [SerializeField] private Vector2Int destroyPriceRange;
+    [SerializeField] private Color[] _colors;
 
     private int destroyPrice;
     private int filling;
+    private SpriteRenderer _spriteRenderer;
 
     public event UnityAction<int> DamageUpdate;
 
@@ -17,9 +21,14 @@ public class Block : MonoBehaviour
 
     private void Start()
     {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
         destroyPrice = Random.Range(destroyPriceRange.x, destroyPriceRange.y);
 
         DamageUpdate?.Invoke(LeftToFill);
+
+
+        SetColor(_colors[Random.Range(0, _colors.Length)]);
     }
 
 
@@ -32,6 +41,12 @@ public class Block : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+
+    private void SetColor(Color color)
+    {
+        _spriteRenderer.color = color;
     }
 
 }
--- Assets/Scripts/WinCanvas.cs
+++ Assets/Scripts/WinCanvas.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class VolumeWin : MonoBehaviour
+public class WinCanvas : MonoBehaviour
 {
     [SerializeField] TMP_Text volumeWin;
 
